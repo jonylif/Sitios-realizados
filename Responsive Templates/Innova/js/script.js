732d5c3fdb7661d30d764e89be3b5f978302c0f4
Jonathan Lifschitz <jony_lif@hotmail.com>
2017-02-28 17:05:57 -0300

    Innova

---
+++ Responsive Templates/Innova/js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    
+    
+    $('.wrapper-works .one').hover(function(){
+        $('.overlay1').fadeIn(400);
+        $('.overlay1 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay1').fadeOut(400);
+        $('.overlay1 p').animate({top: "40px"})
+    });
+    
+        $('.wrapper-works .two').hover(function(){
+        $('.overlay2').fadeIn(400);
+        $('.overlay2 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay2').fadeOut(400);
+        $('.overlay2 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .three').hover(function(){
+        $('.overlay3').fadeIn(400);
+        $('.overlay3 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay3').fadeOut(400);
+        $('.overlay3 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .four').hover(function(){
+        $('.overlay4').fadeIn(400);
+        $('.overlay4 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay4').fadeOut(400);
+        $('.overlay4 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .five').hover(function(){
+        $('.overlay5').fadeIn(400);
+        $('.overlay5 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay5').fadeOut(400);
+        $('.overlay5 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .six').hover(function(){
+        $('.overlay6').fadeIn(400);
+        $('.overlay6 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay6').fadeOut(400);
+        $('.overlay6 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .seven').hover(function(){
+        $('.overlay7').fadeIn(400);
+        $('.overlay7 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay7').fadeOut(400);
+        $('.overlay7 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .eight').hover(function(){
+        $('.overlay8').fadeIn(400);
+        $('.overlay8 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay8').fadeOut(400);
+        $('.overlay8 p').animate({top: "40px"})
+    });
+    
+    $('.wrapper-works .nine').hover(function(){
+        $('.overlay9').fadeIn(400);
+        $('.overlay9 p').animate({top: "0px"})
+    }, function(){
+        $('.overlay9').fadeOut(400);
+        $('.overlay9 p').animate({top: "40px"})
+    });
+    
+    
+});
